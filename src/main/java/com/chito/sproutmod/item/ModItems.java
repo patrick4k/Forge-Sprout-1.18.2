8bb4e784532b7b362dbfa9960edb7ab9cefecc75
patrick4k <pmkennedy4@gmail.com>
2022-05-22 17:48:02 -0400

    mooshaxe added

--- src/main/java/com/chito/sproutmod/item/ModItems.java
+++ src/main/java/com/chito/sproutmod/item/ModItems.java
@@ -1,8 +1,10 @@
 package com.chito.sproutmod.item;
 
 import com.chito.sproutmod.SproutMod;
+import com.chito.sproutmod.item.custom.MooshaxeItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.SnowballItem;
+import net.minecraft.world.item.Tiers;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -16,6 +18,10 @@ public class ModItems {
     // Add items here
     public static final RegistryObject<Item> ROCK = ITEMS.register("rock",
             () -> new SnowballItem(new Item.Properties().tab(ModCreativeModeTab.SPROUT_TAB).stacksTo(16)));
+
+    public static final RegistryObject<Item> MOOSHAXE = ITEMS.register("mooshaxe",
+            () -> new MooshaxeItem(Tiers.NETHERITE, 1, 1,
+                    new Item.Properties().tab(ModCreativeModeTab.SPROUT_TAB).stacksTo(1).durability(100)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
---
+++ src/main/java/com/chito/sproutmod/item/custom/MooshaxeItem.java
@@ -0,0 +1,11 @@
+package com.chito.sproutmod.item.custom;
+
+import net.minecraft.world.item.AxeItem;
+import net.minecraft.world.item.Tier;
+
+public class MooshaxeItem extends AxeItem {
+    public MooshaxeItem(Tier pTier, float pAttackDamageModifier, float pAttackSpeedModifier, Properties pProperties) {
+        super(pTier, pAttackDamageModifier, pAttackSpeedModifier, pProperties);
+    }
+
+}
